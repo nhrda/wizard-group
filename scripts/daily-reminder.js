a710069be682ca3d7724627aa4cfc59fa3a46e1b
nhrda <104025689+nhrda@users.noreply.github.com>
2026-08-18 11:10:06 -0300

    Update daily-reminder.js

--- scripts/daily-reminder.js
+++ scripts/daily-reminder.js
@@ -102,11 +102,12 @@ async function main() {
       service_id: process.env.EMAILJS_SERVICE_ID,
       template_id: process.env.EMAILJS_TEMPLATE_ID,
       user_id: process.env.EMAILJS_PUBLIC_KEY,
+      accessToken: process.env.EMAILJS_PRIVATE_KEY,
       template_params: {
         message_html: html,
         to_email: email
-      }
-    };
+     }
+  };
 
     const res = await fetch('https://api.emailjs.com/api/v1.0/email/send', {
       method: 'POST',
